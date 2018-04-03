b1e49c95689c2f8a6509c53b67d1b5d06b1b2fb0
Tzeusy <uniquosity@gmail.com>
2018-04-04 01:26:23 +0800

    PEP Updates, added 4 test cases.

--- databaseRequestMethods.py
+++ databaseRequestMethods.py
@@ -9,12 +9,12 @@ def getMenu(table_number):
     restaurant_image = "https://i.imgur.com/XvlwlIn.jpg"
 
     with ConnectionFromPool() as cursor:
-        cursor.execute("SELECT column_name,data_type FROM information_schema.columns WHERE table_name = 'menu'");
+        cursor.execute("SELECT column_name,data_type FROM information_schema.columns WHERE table_name = 'menu'")
         menu_headers = cursor.fetchall()
         cursor.execute("SELECT * FROM menu")
-        nonparsed_array = cursor.fetchall()
+        non_parsed_array = cursor.fetchall()
 
-    final_string = jsonify_reply(menu_headers,nonparsed_array)
+    final_string = jsonify_reply(menu_headers, non_parsed_array)
     full_json = '{{"name":"{}","imagehyperlink":"{}","menu":[{}]}}'.format(restaurant_name, restaurant_image, final_string)
     return full_json
 
@@ -24,25 +24,29 @@ def getOrders(customer_id=None):
                   "menu.price, purchases.food_id,purchases.delivered, purchases.comments, purchases.additional_price " \
                   "FROM session INNER JOIN purchases ON purchases.transaction_id = session.transaction_id" \
                   " INNER JOIN menu ON purchases.food_id = menu.food_id "
-    if(customer_id is not None):
-        sql_command+="WHERE session.customer_id = {}".format(customer_id)
+    if customer_id is not None:
+        sql_command += "WHERE session.customer_id = {}".format(customer_id)
 
     with ConnectionFromPool() as cursor:
         # cursor.execute("SELECT column_name,data_type FROM information_schema.columns WHERE table_name = 'session'");
-        menu_headers = [("table_number","integer"),("start_time","timestamp without time zone"),("price","numeric(4,2)"),("food_id","integer"),("delivered","boolean"),("comments","text"),("additional_price","numeric(4,2)")]
+        menu_headers = [("table_number", "integer"), ("start_time", "timestamp without time zone"),
+                        ("price", "numeric(4, 2)"), ("food_id", "integer"), ("delivered", "boolean"),
+                        ("comments", "text"), ("additional_price", "numeric(4,2)")]
         cursor.execute(sql_command)
-        nonparsed_array = cursor.fetchall()
-    final_string = '{{"orders":[{}]}}'.format(jsonify_reply(menu_headers, nonparsed_array))
+        non_parsed_array = cursor.fetchall()
+    final_string = '{{"orders":[{}]}}'.format(jsonify_reply(menu_headers, non_parsed_array))
     return final_string
 
 
-def get_time_and_price(customer_id,table_number):
+def get_time_and_price(customer_id, table_number):
+    print("Getting time and price")
     transaction_id, _, _, _, _ = get_stats(customer_id)
     with ConnectionFromPool() as cursor:
         cursor.execute("SELECT DATE_PART('hour',NOW() - start_time) FROM session WHERE transaction_id = {}".format(
             transaction_id))
         hours = int(cursor.fetchone()[0])
-        cursor.execute("SELECT DATE_PART('minute',NOW() - start_time) FROM session WHERE transaction_id = {}".format(transaction_id))
+        cursor.execute("SELECT DATE_PART('minute',NOW() - start_time) FROM session WHERE transaction_id = {}"
+                       .format(transaction_id))
         minutes = int(cursor.fetchone()[0])
         if 0 < table_number < 50:
             return "{"+'"time_price":{},"hours":{},"minutes":{}'.format(2 * (hours + 1), hours, minutes)+"}"
@@ -55,11 +59,11 @@ def get_time_and_price(customer_id,table_number):
 def get_session():
     sql_command = "SELECT * FROM session"
     with ConnectionFromPool() as cursor:
-        cursor.execute("SELECT column_name,data_type FROM information_schema.columns WHERE table_name = 'session'");
+        cursor.execute("SELECT column_name,data_type FROM information_schema.columns WHERE table_name = 'session'")
         session_headers = cursor.fetchall()
         cursor.execute(sql_command)
         session_array = cursor.fetchall()
-    final_string = '{{"session":[{}]}}'.format(jsonify_reply(session_headers,session_array))
+    final_string = '{{"session":[{}]}}'.format(jsonify_reply(session_headers, session_array))
     return final_string
 
 
@@ -69,35 +73,36 @@ def jsonify_reply(headers, row_information):
     # Presumes headers is in [(column_name,data_type),...] format
     # Presumes row information is in [(elem1, elem2,..n),...] format where n=number of columns
 
-    infoArray = []
+    info_array = []
     for i in range(len(row_information)):
-        elementInfo = "{"
+        element_info = "{"
         for j in range(len(headers)):
-            #LEFT ELEMENT: Header will definitely be in string format
-            elementInfo+='"'
-            elementInfo+=headers[j][0]
-            elementInfo+='"'
+            # LEFT ELEMENT: Header will definitely be in string format
+            element_info += '"'
+            element_info += headers[j][0]
+            element_info += '"'
 
-            elementInfo+=":"
-            #RIGHT ELEMENT: Data may not necessarily be in string format.
-            #May need to add apostrophes to the column information, if it is a STRING or a ENUM
-            if(headers[j][1]=='text' or headers[j][1]=='USER-DEFINED' or 'timestamp' in headers[j][1]):
-                elementInfo+='"'
-            #True needs to be converted to true, same for False to false >.>
-            if(headers[j][1]=='boolean'):
-                elementInfo+=str(row_information[i][j]).lower()
+            element_info += ":"
+            # RIGHT ELEMENT: Data may not necessarily be in string format.
+            # May need to add apostrophes to the column information, if it is a STRING or a ENUM
+            if headers[j][1] == 'text' or headers[j][1] == 'USER-DEFINED' or 'timestamp' in headers[j][1]:
+                element_info += '"'
+            # True needs to be converted to true, same for False to false >.>
+            if headers[j][1] == 'boolean':
+                element_info += str(row_information[i][j]).lower()
             else:
-                elementInfo += str(row_information[i][j])
-            if (headers[j][1] == 'text' or headers[j][1] == 'USER-DEFINED' or 'timestamp' in headers[j][1]):
-                elementInfo += '"'
-            if(j!=len(headers)-1):
-                elementInfo+=","
-        elementInfo+="}"
-        infoArray.append(elementInfo)
-    finalReply = ",".join(element for element in infoArray)
-    return finalReply
+                element_info += str(row_information[i][j])
+            if headers[j][1] == 'text' or headers[j][1] == 'USER-DEFINED' or 'timestamp' in headers[j][1]:
+                element_info += '"'
+            if j != len(headers)-1:
+                element_info += ","
+        element_info += "}"
+        info_array.append(element_info)
+    final_reply = ",".join(element for element in info_array)
+    return final_reply
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print(getMenu(49))
     # myNiceText = json.loads(get_session(49))
     # print(json.dumps(myNiceText, sort_keys=True,indent = 4, separators = (',', ': ')))
--- testapp.py
+++ testapp.py
@@ -1,6 +1,9 @@
+import string
+
 import requests
 import unittest
 import json
+import random
 
 def is_json(myjson):
   try:
@@ -45,6 +48,10 @@ class MenuTestCase(TestServerMethods):
 
 class CreateUser(TestServerMethods):
     def test_create_non_crooked_cooks_user(self):
+        # Exit in case they already exist
+        exit_address = self.base_address + "make_payment?plid=56789&token_id=tok_visa"
+        r = requests.get(exit_address)
+
         print("Testing user for table number 50 <= n < 100")
         register_address = self.base_address + "register?plid=56789&table_number=51"
         print("Visiting " + register_address)
@@ -53,7 +60,7 @@ class CreateUser(TestServerMethods):
         self.assertEqual(str(r.status_code),"200")
         self.assertEqual(response, "False")
 
-        #Creating an identical user after first exists should not be possible
+        # Creating an identical user after first exists should not be possible
         print("Testing repeat visit, if previous session has not closed")
         r = requests.get(register_address)
         print("Visiting " + register_address)
@@ -61,14 +68,18 @@ class CreateUser(TestServerMethods):
         self.assertEqual(str(r.status_code), "200")
         self.assertEqual(response, "Invalid - entry for customer already exists in table")
 
-        #Make them exit so we can run this test automatically
-        exit_address = self.base_address + "make_payment?plid=56789&token_id=tok_visa"
+        # Make them exit so we can run this test automatically
+        # exit_address = self.base_address + "make_payment?plid=56789&token_id=tok_visa"
         r = requests.get(exit_address)
         print("User exiting restaurant")
         print("Visiting " + exit_address)
         response = r.content.decode("UTF-8")
 
     def test_create_crooked_cooks_user(self):
+        # Exit in case they already exist
+        exit_address = self.base_address + "make_payment?plid=23456&token_id=tok_visa"
+        r = requests.get(exit_address)
+
         print("Testing user for table number 0 < n < 50")
         register_address = self.base_address + "register?plid=23456&table_number=37&num_people=3"
         r = requests.get(register_address)
@@ -102,9 +113,30 @@ class CreateUser(TestServerMethods):
         self.assertEqual(response, "True")
 
 
-class TestStatusCodes(TestServerMethods):
+class UserExperience(TestServerMethods):
+    def setUp(self):
+        self.base_address = "http://crookedcooks.herokuapp.com/api/"
+        print("Creating User 000234")
+        register_address = self.base_address + "register?plid=000234&table_number=1&num_people=3"
+        print("Visiting " + register_address)
+        r = requests.get(register_address)
+        self.assertEqual(str(r.status_code), "200")
+        order_address = self.base_address + "make_order?plid=000234"
+        print("Visiting " + order_address)
+        r = requests.post(order_address, json={
+            "orders": [{"food_id": "103", "comment": "testing"},
+                       {"food_id": "203", "comment": "onetwothree"},
+                       {"food_id": "101", "comment": "SADFSDFSDHFDSSDF"}]})
+        self.assertEqual(str(r.status_code), "200")
+
+    def tearDown(self):
+        payment_address = self.base_address + "make_payment?plid=000234&token_id=tok_visa"
+        print("Visiting " + payment_address)
+        r = requests.get(payment_address)
+        self.assertEqual(str(r.status_code), "200")
+
     def test_session(self):
-        print("Testing session page")
+        print("Testing status of session page")
         session_address = self.base_address + "get_session"
         print("Visiting " + session_address)
         r = requests.get(session_address)
@@ -118,37 +150,32 @@ class TestStatusCodes(TestServerMethods):
         self.assertTrue(is_json(r.content.decode("utf-8")))
 
     def test_existing_orders(self):
-        print("Testing existing orders page")
+        print("Testing status of existing orders page")
         orders_page = self.base_address + "existing_orders"
         print("Visiting " + orders_page)
         r = requests.get(orders_page)
         self.assertEqual(str(r.status_code), "200")
 
     def test_existing_orders_json(self):
-        print("Testing existing orders page")
+        print("Testing existing orders's JSON is valid")
         orders_page = self.base_address + "existing_orders"
         print("Visiting " + orders_page)
         r = requests.get(orders_page)
         self.assertTrue(is_json(r.content.decode("utf-8")))
 
-    def test_query_price(self):
-        print("Testing query price")
-        print("Creating User 000234")
-        register_address = self.base_address + "register?plid=000234&table_number=1&num_people=3"
-        r = requests.get(register_address)
-        print("Visiting " + register_address)
-        print("User created; now making orders")
-        order_address = self.base_address + "make_order?plid=000234"
-        print("Visiting " + order_address)
-        r = requests.post(order_address, json={
-            "orders": [{"food_id": "103", "comment": "testing"},
-                       {"food_id": "203", "comment": "onetwothree"},
-                       {"food_id": "101", "comment": "SADFSDFSDHFDSSDF"}]})
+    def test_existing_individual_order(self):
+        orders_page = self.base_address + "existing_orders?plid=000234"
+        print("Visiting " + orders_page)
+        r = requests.get(orders_page)
         self.assertEqual(str(r.status_code), "200")
-        print("Orders made; retrieving orders")
-        existing_orders_address = self.base_address + "existing_orders?plid=000234"
-        print("Visiting " + existing_orders_address)
-        r = requests.get(existing_orders_address)
+
+    def test_existing_individual_order_json(self):
+        orders_page = self.base_address + "existing_orders?plid=000234"
+        print("Visiting " + orders_page)
+        r = requests.get(orders_page)
+        self.assertTrue(is_json(r.content.decode("UTF-8")))
+
+    def test_query_price(self):
         print("Testing price of his orders")
         price_address = self.base_address + "query_price?plid=000234"
         print("Visiting " + price_address)
@@ -157,11 +184,123 @@ class TestStatusCodes(TestServerMethods):
         price_information = json.loads(r.content.decode("UTF-8").replace("'", '"'))
         print("Price of his order is " + str(price_information['total_price']) + ", should be 15.4")
         self.assertEqual(price_information['total_price'], 15.4)
-        print("Testing exiting restaurant")
-        payment_address = self.base_address + "make_payment?plid=000234&token_id=tok_visa"
-        print("Visiting " + payment_address)
-        r = requests.get(payment_address)
+
+    def test_get_time_price(self):
+        print("Testing status of get_time_price")
+        time_price_link = self.base_address + "get_time_price?plid=000234&table_number=1"
+        r = requests.get(time_price_link)
         self.assertEqual(str(r.status_code), "200")
+
+    def test_get_time_price_json(self):
+        print("Testing time_price")
+        time_price_link = self.base_address + "get_time_price?plid=000234&table_number=1"
+        r = requests.get(time_price_link)
+        time_and_price = json.loads(r.content.decode("UTF-8").replace("'",'"'))
+        self.assertTrue(is_json(json.dumps(time_and_price)))
+
+    def test_set_delivered(self):
+        print("Setting set_delivered functionality")
+        orders_link = self.base_address + "existing_orders?plid=000234"
+        r = requests.get(orders_link)
+        previous_order_statuses = r.content.decode("UTF-8")
+        print("Before: Orders are " + previous_order_statuses)
+        print("Making one instance of order 103 delivered")
+        delivered_link = self.base_address + "admin/set_delivered?plid=000234&food_id=103"
+        r = requests.get(delivered_link)
+        r = requests.get(orders_link)
+        later_order_statuses = r.content.decode("UTF-8")
+        print("After: Orders are " + later_order_statuses)
+        # Crude; but about same speed as doing a dictionary iteration
+        self.assertTrue('"food_id":103,"delivered":false' in previous_order_statuses)
+        self.assertTrue('"food_id":103,"delivered":true' in later_order_statuses)
+        print("Set_delivered test passed")
+
+    def test_set_availability(self):
+        print("Testing set_available functionality within the menu")
+        make_available_link = self.base_address + "admin/set_availability?boolean=true&food_id=102"
+        make_unavailable_link = self.base_address + "admin/set_availability?boolean=false&food_id=102"
+        # Initialize as true
+        requests.get(make_available_link)
+
+        menu_link = self.base_address + "menu?table_number=49"
+        r = requests.get(menu_link)
+        menu = r.content.decode("UTF-8").replace("'", '"')
+        menu_items = json.loads(menu)['menu']
+        for item in menu_items:
+            if item['food_id'] == 102:
+                # Check if originally true
+                self.assertEqual(item['is_available'], True)
+        # Set to unavailable
+        requests.get(make_unavailable_link)
+        r = requests.get(menu_link)
+        menu = r.content.decode("UTF-8").replace("'", '"')
+        menu_items = json.loads(menu)['menu']
+        for item in menu_items:
+            if item['food_id'] == 102:
+                # Check if false
+                self.assertEqual(item['is_available'], False)
+        # Set to available
+        requests.get(make_available_link)
+        r = requests.get(menu_link)
+        menu = r.content.decode("UTF-8").replace("'", '"')
+        menu_items = json.loads(menu)['menu']
+        for item in menu_items:
+            if item['food_id'] == 102:
+                # Check if true
+                self.assertEqual(item['is_available'], True)
+
+
+class ExtremityTests(TestServerMethods):
+    def register_numbers(self):
+        customer_id_possibilities = [1,500000,100000000000]
+        for customer_id in customer_id_possibilities:
+            exit_address = self.base_address + "make_payment?plid={}&token_id=tok_visa".format(customer_id)
+            r = requests.get(exit_address)
+            register_address = self.base_address + "register?plid={}&table_number=51".format(customer_id)
+            print("Visiting " + register_address)
+            r = requests.get(register_address)
+            response = r.content.decode("UTF-8")
+            self.assertEqual(str(r.status_code), "200")
+            self.assertEqual(response, "False")
+            # Creating an identical user after first exists should not be possible
+            print("Testing repeat visit, if previous session has not closed")
+            r = requests.get(register_address)
+            print("Visiting " + register_address)
+            response = r.content.decode("UTF-8")
+            self.assertEqual(str(r.status_code), "200")
+            self.assertEqual(response, "Invalid - entry for customer already exists in table")
+            # Make them exit so we can run this test automatically
+            # exit_address = self.base_address + "make_payment?plid=56789&token_id=tok_visa"
+            r = requests.get(exit_address)
+            print("User exiting restaurant")
+            print("Visiting " + exit_address)
+            response = r.content.decode("UTF-8")
+
+    def test_user_input(self):
+        exit_address = self.base_address + "make_payment?plid={}&token_id=tok_visa".format("000234")
+        self.base_address = "http://crookedcooks.herokuapp.com/api/"
+        print("Creating User 000234")
+        register_address = self.base_address + "register?plid=000234&table_number=1&num_people=3"
+        print("Visiting " + register_address)
+        r = requests.get(register_address)
+        self.assertEqual(str(r.status_code), "200")
+        order_address = self.base_address + "make_order?plid=000234"
+        print("Visiting " + order_address)
+        comments = []
+        for i in range(3):
+            string_length = random.randrange(0,1000)
+            # Random string lengths of up to 1000 chars
+            comments.append(''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(string_length)))
+        r = requests.post(order_address, json={
+            "orders": [{"food_id": "103", "comment": comments[0]},
+                       {"food_id": "203", "comment": comments[1]},
+                       {"food_id": "101", "comment": comments[2]}]})
+        self.assertEqual(str(r.status_code), "200")
+        orders_page = self.base_address + "existing_orders?plid=000234"
+        r = requests.get(orders_page)
+        print(r.content.decode("UTF-8"))
+        requests.get(exit_address)
+
 
 
 if __name__=="__main__":
